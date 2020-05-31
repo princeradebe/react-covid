43c6073f9ebe72b7f00c1aa3d79540324798c1c9
Prince <princeradebe@gmail.com>
2020-05-31 18:29:21 +0200

    Add search functionality to filter mosnters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters : []
+      monsters : [], 
+      searchField: ""
     };
   }
 
@@ -17,9 +19,14 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter( monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className="App">
-      <CardList monsters={ this.state.monsters } />
+      <SearchBox placeholder= 'search monsters' handleChange={e => this.setState({ searchField: e.target.value })} />
+      <CardList monsters={ filteredMonsters } />
     </div>
     );
   }
